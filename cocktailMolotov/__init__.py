bf62d8b87e00e21bb8d25715c5af819b3966a12a
Matt Nannetti <mattnannetti@gmail.com>
2020-12-09 16:32:06 +0100

    ok

--- cocktailMolotov/__init__.py
+++ cocktailMolotov/__init__.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import os
-from flask import Flask
-from flask_login import LoginManager
-=======
 #import os
 #from flask import Flask
->>>>>>> 48753930236045b0a9e256fc6e091ce2026d004f
 
 #app = Flask(__name__)
 
