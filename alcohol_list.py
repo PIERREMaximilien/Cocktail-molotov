7d37fe01f19d4078fb2815efc0afbc10f41ed770
Alex <alex.spiesberger@gmail.com>
2020-12-14 16:22:08 +0100

    Changing some things in the json

---
+++ alcohol_list.py
@@ -0,0 +1,34 @@
+"""
+Alcohols list :
+    -vodka
+    -rum
+    -gin
+    -whiskey
+    -cachaca
+    -coffee liqueur
+    -triple sec
+    -bourbon
+    -tequila
+    -absinthe
+    -dry vermouth
+    -sweet red vermouth
+    -campari
+    -creme de mure
+    -cherry heering
+    -benedictine
+    -chambord
+    -kahlua
+    -strawberry liqueur
+    -creme de cacao
+    -pisco
+    -champagne
+    -creme de banane
+    -peach schnapps
+    -prosecco
+    -cognac
+    -passoa
+    -black raspberry liqueur
+    -brandy
+"""
+# Loop through the ingredients
+# change Ingredients -> only rum (ex) or do if ex: rum in ingredients
