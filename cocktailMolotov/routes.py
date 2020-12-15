d9d5f620090e4a55e592160995ed1095187ff433
Matt Nannetti <mattnannetti@gmail.com>
2020-12-15 10:22:47 +0100

    allCocktails delete, profile info added

--- cocktailMolotov/routes.py
+++ cocktailMolotov/routes.py
@@ -11,13 +11,11 @@ from cocktailMolotov import cocktails as api
 @app.route('/')
 @app.route('/home')
 def home():
+    return render_template('home.html', title="Home")7
+
+@app.route('/home/<search-result=>')
+def home():
     return render_template('home.html', title="Home")
-
-
-
-@app.route('/allcocktails')
-def allcocktails():
-    return render_template('allCocktails.html', title='All Cocktails')
 
 
 @app.route('/mycocktails')
@@ -91,40 +89,3 @@ def api_all():
     print(result)
     #return 'OK'
     return jsonify(result)
-
-
-'''
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', title="Home")
-
-@app.route('/allcocktails')
-def allcocktails():
-    return render_template('allCocktails.html', title='All Cocktails')
-
-@app.route('/mycocktails')
-def mycocktails():
-    return render_template('myCocktails.html', title='My Cocktails')
-
-@app.route('/singlecocktail')
-def singlecocktail():
-    return render_template('singlecocktail.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html', title='My Profile')
-
-@app.route('/register')
-def register():
-    return render_template('register.html', title='Register')
-
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Log in')
-
-@app.route('/api/v1/ressources/cocktails/all', methods=['GET'])
-def api_all():
-    return jsonify(cocktails)
-'''
